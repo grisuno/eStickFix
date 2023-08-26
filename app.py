505752884f04bd9ba1e4a6852c4702fba571c535
Grisuno <grisuno@gmail.com>
2023-08-26 02:14:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,27 @@
-import os
-import yaml
-import pandas as pd
-import paramiko
-import pymysql
-import logging
-from ftplib import FTP
-from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_security import Security, SQLAlchemyUserDatastore, RoleMixin
 from data_navigation import DataNavigator
 from etl import Config, Transformer, DataQualityChecker, ETLProcessor
-from data_loader import SourceLoader
 
-def main():
-    with open("sources.yaml", "r") as sources_file:
-        sources_config = yaml.safe_load(sources_file)['sources']
-    
-    loader = SourceLoader(sources_config)
-    loader.load_sources()
-    
-    logging.basicConfig(filename="log/orchestration_log.log", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-    
-    config_files = ["config/config1.yaml", "config/config2.yaml", "config/config3.yaml"]
-    
-    for config_file in config_files:
-        config = Config(config_file)
-        transformation_script = f"transformation/{config.get('dataset')}/{config.get('tablename')}/{config.get('date_now')}/script.py"
-        qa_script = f"qa/{config.get('dataset')}/{config.get('tablename')}/{config.get('date_now')}/qa_script.py"
-        
-        transformer = Transformer(transformation_script)
-        quality_checker = DataQualityChecker(qa_script)
-        
-        data_navigator = DataNavigator("mydatabase", "etl_log")
-        
-        etl_processor = ETLProcessor(config, transformer, quality_checker, data_navigator)
-        etl_processor.process()
-    
-    data_nav = DataNavigator("mydatabase", "etl_log")
-    dataset = "dataset1"
-    table_name = "table1"
-    
-    data = data_nav.find_by_dataset_tablename_date(dataset, table_name, datetime.now().strftime('%Y%m%d'))
-    print("Data:")
-    print(data)
-    
-    query = {"dataset": dataset, "tablename": table_name}
-    result = data_nav.find_by_query(query)
-    print("Result:")
-    for item in result:
-        print(item)
-    
-    df = pd.DataFrame(result)
-    print("Statistics:")
-    print(df.describe())
-    print("Correlation:")
-    print(df.corr())
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+
+login_manager = LoginManager(app)
+data_navigator = DataNavigator("mydatabase", "etl_log")
+
+# Define your SQLAlchemy models for users and roles here
+# ...
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+# Implement other views for creating config files, controlling ETL, etc.
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
