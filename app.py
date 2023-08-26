45d787a3fed8583e5a532024f0a717bfe67855cd
Grisuno <grisuno@gmail.com>
2023-08-26 02:14:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,12 +16,71 @@ data_navigator = DataNavigator("mydatabase", "etl_log")
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
+class ETLController:
+    def __init__(self, user):
+        self.user = user
+    
+    @login_required
+    def start_etl(self, config_file):
+        # Check user role and permissions
+        
+        config = Config(config_file)
+        transformation_script = f"transformation/{config.get('dataset')}/{config.get('tablename')}/{config.get('date_now')}/script.py"
+        qa_script = f"qa/{config.get('dataset')}/{config.get('tablename')}/{config.get('date_now')}/qa_script.py"
+        
+        transformer = Transformer(transformation_script)
+        quality_checker = DataQualityChecker(qa_script)
+        
+        etl_processor = ETLProcessor(config, transformer, quality_checker, data_navigator)
+        etl_processor.process()
+
+class UserController:
+    def __init__(self, user):
+        self.user = user
+    
+    @login_required
+    def create_config(self, config_data):
+        # Check user role and permissions
+        
+        # Save config_data to a config file
+
+class ProcessController:
+    def __init__(self, user):
+        self.user = user
+    
+    @login_required
+    def check_etl_status(self):
+        # Check user role and permissions
+        
+        # Get ETL status information from logs
+
 @app.route("/")
 @login_required
 def index():
     return render_template("index.html")
 
-# Implement other views for creating config files, controlling ETL, etc.
+@app.route("/create_config", methods=["POST"])
+@login_required
+def create_config():
+    config_data = request.form  # Form data for creating config
+    user_controller = UserController(current_user)
+    user_controller.create_config(config_data)
+    return redirect(url_for("index"))
+
+@app.route("/start_etl/<string:config_file>")
+@login_required
+def start_etl(config_file):
+    etl_controller = ETLController(current_user)
+    etl_controller.start_etl(config_file)
+    flash("ETL process started successfully!", "success")
+    return redirect(url_for("index"))
+
+@app.route("/etl_status")
+@login_required
+def etl_status():
+    process_controller = ProcessController(current_user)
+    status_info = process_controller.check_etl_status()
+    return render_template("etl_status.html", status_info=status_info)
 
 if __name__ == "__main__":
     app.run(debug=True)
